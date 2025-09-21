06ae6637f8715b7c8a8ae831f38729bc0cd851da
Md Shamim Mia <rakibkhanshamim790@gmail.com>
2025-09-21 20:37:45 +0600

    fix legacy peer

---
+++ site-setting.js
@@ -0,0 +1,8 @@
+const SiteSettings = {
+  siteName: "ES Vibes",
+  siteUrl: "https://esvibes.com",
+  siteDescription:
+    "Your go-to source for the latest in electronic music, news, reviews, and event coverage.",
+  siteKeywords:
+    "electronic music, EDM, music news, music reviews, event coverage, DJ interviews",
+};
